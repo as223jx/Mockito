81e1a04a48eea70d9f55f1f5933fef98943acce5
as223jx <as223jx@student.lnu.se>
2015-12-14 12:56:42 +0100

    REFACTOR

--- src/mockito/HandleArray.java
+++ src/mockito/HandleArray.java
@@ -2,10 +2,17 @@ package mockito;
 
 public class HandleArray {
 	
+	private int[] intArr;
+	
 	public HandleArray(int[] arr) {
+		intArr = arr;
 	}
 	
 	public int addValues() {
-		return 0;
+		int sum = 0;
+		for(int i = 0; i < intArr.length; i++) {
+			sum += intArr[i];
+		}
+		return sum;
 	}
 }
