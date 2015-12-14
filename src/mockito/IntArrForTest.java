780904269953332526d9daed43b5ab6e85100a58
as223jx <as223jx@student.lnu.se>
2015-12-14 13:04:52 +0100

    GREEN

---
+++ src/mockito/IntArrForTest.java
@@ -0,0 +1,12 @@
+package mockito;
+
+public class IntArrForTest {
+	
+	public IntArrForTest(int a, int b, int c, int d) {
+		
+	}
+	
+	public int[] getIntArr() {
+		return new int[] { 1, 2, 3, 4};
+	}
+}
